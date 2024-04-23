fab4d717658c8bc3b48f1defa5e0411f5cb01f61
Lucas Lara <lucaslara99@gmail.com>
2024-04-23 08:44:56 +0200

    App.tsx reformat

--- projects/hola-mundo/src/App.tsx
+++ projects/hola-mundo/src/App.tsx
@@ -12,9 +12,21 @@ export function App() {
                 name='Lucas'
                 userName='lucario'
             />
-            <TwitterFollowCard formatUsername={formatUsername} name="Nacho" userName="nachete45" />
-            <TwitterFollowCard formatUsername={formatUsername} name={'Luisardo'} userName={'Luisete'} />
-            <TwitterFollowCard formatUsername={formatUsername} name={"Lucia"} userName={"Lusilu99"} />
+            <TwitterFollowCard
+                formatUsername={formatUsername}
+                name="Nacho"
+                userName="nachete45"
+            />
+            <TwitterFollowCard
+                formatUsername={formatUsername}
+                name={'Luisardo'}
+                userName={'Luisete'}
+            />
+            <TwitterFollowCard
+                formatUsername={formatUsername}
+                name={"Lucia"}
+                userName={"Lusilu99"}
+            />
         </section>
     )
 }
